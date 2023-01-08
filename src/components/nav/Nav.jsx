192e89d3dea6ee7f63dfbc657d3a4dd34e0c8f80
Luís Melo <luis.f.duraes.melo@gmail.com>
2023-01-08 17:47:02 +0000

    Navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,23 +5,46 @@ import { AiOutlineUser } from 'react-icons/ai';
 import { BiBook } from 'react-icons/bi';
 import { BsGear } from 'react-icons/bs';
 import { BiMessageDetail } from 'react-icons/bi';
+import { useState } from 'react';
 
 const Nav = () => {
+  const [active, setActive] = useState('#');
+
   return (
     <nav>
-      <a href="#">
+      <a
+        href="#"
+        onClick={() => setActive('#')}
+        className={active === '#' ? 'active' : ''}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActive('#about')}
+        className={active === '#about' ? 'active' : ''}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActive('#experience')}
+        className={active === '#experience' ? 'active' : ''}
+      >
         <BiBook />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => setActive('#services')}
+        className={active === '#services' ? 'active' : ''}
+      >
         <BsGear />
       </a>
-      <a href="#contact">
+      <a
+        href="#contacts"
+        onClick={() => setActive('#contacts')}
+        className={active === '#contacts' ? 'active' : ''}
+      >
         <BiMessageDetail />
       </a>
     </nav>
